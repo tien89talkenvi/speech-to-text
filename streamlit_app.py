2520da89c25acbf73c481872f965241d603e9692
tien89talkenvi <147873351+tien89talkenvi@users.noreply.github.com>
2023-11-16 03:37:46 +0700

    Add files via upload

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,16 +1,45 @@
-import streamlit as st 
+# https://tien89.streamlit.app/     # up ngay 15-11-2023
+
+import streamlit as st
 import pyttsx3
+import base64
+import time
 
-engine = pyttsx3.init()
+class TextToSpeech:
+    def __init__(self):
+        self.engine = pyttsx3.init()
+        self.engine.setProperty('rate', 125)
 
-# Lấy danh sách giọng nói
-voices = engine.getProperty('voices')
+        voices = self.engine.getProperty('voices')
+        self.engine.setProperty('voice', voices[1].id) # female voice (1)
 
-butt=st.button('click')
+    def convert(self, text:str, filename: str="hello.mp3"):
+        self.engine.save_to_file(text, filename)
+        self.engine.runAndWait()
 
-if butt:
-    # In thông tin về từng giọng nói
-    for voice in voices:
-        # print(f"ID: {voice.id}, Name: {voice.name}, Lang: {voice.languages}")
-        st.write(f"ID: {voice.id}, Name: {voice.name}, Lang: {voice.languages}")
+        print(f"Saved audio to {filename}")
+
+###########################
+txt2speech = TextToSpeech()
+
+st.title('Text To Speech Example')
+
+conversion_text = st.text_input('Text to convert', 'I am Vietnamese. I am from Saigon.')
+
+if st.button('Convert'):
+    txt2speech.convert(text=conversion_text)
+    audio_placeholder=st.empty()
+    with open('hello.mp3', 'rb') as audio_file:
+        audio_bytes = audio_file.read()
+    #st.audio(audio_bytes, format='audio/mp3')
+    audio_b64 = base64.b64encode(audio_bytes).decode()
+    my_html=f'''
+            <audio controls autoplay>
+            <source src="data:audio/mp3;base64,{audio_b64}">
+            </audio>
+            '''
+    audio_placeholder.empty()
+    time.sleep(0.2)
+    #html(my_html) 
+    audio_placeholder.markdown(my_html,unsafe_allow_html=True)
 
