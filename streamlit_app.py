83ecdd28b7c4418e831f1de8b2a062a47a402347
tien89talkenvi <147873351+tien89talkenvi@users.noreply.github.com>
2024-02-29 16:11:39 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,4 +1,3 @@
-# pip install youtube-transcript-api
 import streamlit as st
 import streamlit.components.v1 as components 
 import time
@@ -8,44 +7,59 @@ from urllib import parse
 import json
 
 
-#------------------------------------
-@st.cache_data
-def is_from_youtube(url:str) -> bool:
-    youtube_regex = (
-        r'(https?://)?(www\.)?'
-        '(youtube|youtu|youtube-nocookie)\.(com|be)/'
-        '(watch\?v=|embed/|v/|.+\?v=)?([^&=%\?]{11})')
-    return re.match(youtube_regex, url)
-
-#--------------------------------------
-@st.cache_data
-def get_youtube_id(url:str) -> str:
-    query = parse.urlparse(url)
-    if query.hostname == 'youtu.be':
-        return query.path[1:]
-    if query.hostname in ('www.youtube.com', 'youtube.com'):
-        if query.path == '/watch':
-            p = parse.parse_qs(query.query)
-            return p['v'][0]
-        if query.path[:7] == '/embed/':
-            return query.path.split('/')[2]
-        if query.path[:3] == '/v/':
-            return query.path.split('/')[2]
-    return ""
-
 #------------------------------------------
-@st.cache_data
 def Lay_ttin_cua_url(url_vid_input):
-    id_ofvid = get_youtube_id(url_vid_input)
-    transcript_list = YouTubeTranscriptApi.list_transcripts(id_ofvid)
-    for transcript in transcript_list:
-        return transcript.translate('vi').fetch()
+    id_ofvid = extract.video_id(url_vid_input)
+    try:
+        transcript_list = YouTubeTranscriptApi.list_transcripts(id_ofvid)
+        if transcript_list:
+            transcript = YouTubeTranscriptApi.get_transcript(id_ofvid)
+            return transcript
+    except:    
+        return ''
+    #transcript_list = YouTubeTranscriptApi.list_transcripts(id_ofvid)
+    #for transcript in transcript_list:
+        #st.write(
+            # id cua video
+        #    transcript.video_id,
+            # ngon ngu cua text
+        #    transcript.language,
+            # ma code ngon ngu goc
+        #    transcript.language_code,
+            # duoc tao binh thuong hay sinh ra tu Yt
+        #    transcript.is_generated,
+            # da duoc dich ra en tu goc hay la khong
+        #    transcript.is_translatable,
+            # day la ban dich ra ngon ngu nao 
+        #    transcript.translation_languages,
+        #)
+
+        # fetch the actual transcript data
+        #print(transcript.fetch())
+
+        # translating the transcript will return another
+        # transcript object
+        #return transcript.translate('vi').fetch()
+        #print(transcript.translate('vi').fetch())
+
+    # you can also directly filter for the language you are
+    # looking for, using the transcript list
+    #transcript = transcript_list.find_transcript(['en'])
+    #return transcript 
+    # or just filter for manually created transcripts
+    #transcript = transcript_list.find_manually_created_transcript(['en'])
 #--------------------------------------------------------
 @st.cache_data
 def Dem_txtbig_vao_html(fulltxt):
     ltext = fulltxt.split('.')
     chp='<br><br>'
     chp=chp+'<div class="f-grid">'+fulltxt+'</div>'   
+    js1='''
+        function googleTranslateElementInit(){new google.translate.TranslateElement({pageLanguage:'en'}, 'google_translate_element');}
+        '''
+    js2='''
+        src="//translate.google.com/translate_a/element.js?cb=googleTranslateElementInit"
+        '''
     js3='''
         function speak_text_all(idname){
         //  khoi dong text_all la bien gloabal  
@@ -84,15 +98,19 @@ def Dem_txtbig_vao_html(fulltxt):
                 <meta charset="UTF-8">
                 <meta http-equiv="X-UA-Compatible" content="IE=edge">
                 <meta name="viewport" content="width=device-width, initial-scale=1.0">
-                <title>Tiensg89's App</title>
+                <title>Read Video</title>
                 <style> {sty} </style>
                 </head>
                 <body>
                 <iframe width="100%" height="460" src="{url_vid_input}" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
                 <hr>
-                <button id="English" onclick="speak_text_all(this.id)">Speak with English</button>
-                <button id="Vietnamese" onclick="speak_text_all(this.id)">Speak with Vietnamese</button>
-                <button id="stop" onclick="stop()">Stop</button>
+                <div id="google_translate_element" ></div>
+                <script>{js1}</script>
+                <script {js2}></script>
+ 
+                <button id="English" onclick="speak_text_all(this.id)" translate="no">Speak with English</button>
+                <button id="Vietnamese" onclick="speak_text_all(this.id)" translate="no">Speak with Vietnamese</button>
+                <button id="stop" onclick="stop()" translate="no">Stop</button>
                 <script>{js3}</script>
                 <script>{js4}</script>
             
@@ -101,33 +119,28 @@ def Dem_txtbig_vao_html(fulltxt):
                 <br>
                 </body>
                 </html>
-                """,height=600,scrolling=True)
+                """,height=640,scrolling=True)
 
 #==============================================================================
 st.title('Nghe Văn bản của Video Youtube')
-#video_input = st.text_input(':green[Nhập URL của youtube rồi Enter. Ví dụ : https://www.youtube.com/watch?v=KG0Q05Lnm7s]',key='IP1')
-url_vid_input = st.text_input(':green[Nhập URL của youtube rồi Submit]', value = 'https://www.youtube.com/embed/lcZDWo6hiuI',key='IP1')
+url_vid_input = st.text_input(':green[Nhập URL của youtube rồi Enter. Ví dụ : https://www.youtube.com/embed/lcZDWo6hiuI]',key='IP1')
+#url_vid_input = st.text_input(':green[Nhập URL của youtube rồi Submit]', value = 'https://www.youtube.com/watch?v=KG0Q05Lnm7s',key='IP1')
 if st.checkbox('Submit'):
     # neu video_input (url) hop le va cai nay cua youtube 
-    if url_vid_input and is_from_youtube:
+    if url_vid_input :
+        
         vanban_phienam = Lay_ttin_cua_url(url_vid_input)
+        if vanban_phienam != '':
+            text_all = ''
+            for element in vanban_phienam:
+                text_element = element['text']
+                text_all = text_all + text_element + ' '
 
-        #thu
-        filename = 'zzzthu'
-        _id = get_youtube_id(url_vid_input)
-        transcript = YouTubeTranscriptApi.get_transcript(_id)
-        with open(f'{filename}.json', 'w', encoding='utf-8') as json_file:
-            json.dump(transcript, json_file)
-        #    
-         
-        text_all = ''
-        for element in vanban_phienam:
-            text_element = element['text']
-            text_all = text_all + text_element + ' '
-
-        Dem_txtbig_vao_html(text_all)
-        st.success('Converting Complete', icon="✅")
-        st.balloons()
+            Dem_txtbig_vao_html(text_all)
+            st.success('Converting Complete', icon="✅")
+            st.balloons()
+        else:
+            st.write(':orange[Không có bản phiên âm nào!]')        
     else:
-        st.write(':orange[Chưa nhập url hoặc url này không phải của Youtube!]')
+        st.write(':orange[Chưa nhập url hoặc url này không hợp lệ!]')
 
